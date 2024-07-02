1ba86b79e17adff70ee5add0354d274a6212c47d
Andrey Sheshko <116847097+apachaihop@users.noreply.github.com>
2024-07-02 12:26:52 +0300

    Update EurekaClientApplication.java

--- eureka-client/src/main/java/com/example/eureka_client/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eureka_client/EurekaClientApplication.java
@@ -2,10 +2,17 @@ package com.example.eureka_client;
 
 
 import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -19,4 +26,15 @@ public class EurekaClientApplication {
                 .singletonMap("server.port", PORT));
         app.run(args);
     }
+    @Bean
+        public CorsFilter corsFilter() {
+            final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            final CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true);
+            config.setAllowedOrigins(Collections.singletonList("*"));
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+            source.registerCorsConfiguration("/**", config);
+            return new CorsFilter(source);
+        }
 }
